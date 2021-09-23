d046c4692920f2da1f06914d82d1a85e121e7dbe
ankkyprasad <ankkyprasad@gmail.com>
2021-09-23 19:52:09 +0530

    added mongodb and redis support

--- day11/routes/index.js
+++ day11/routes/index.js
@@ -5,7 +5,14 @@ var register = require("../controllers/register");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  const sess = req.session;
+  sess.username = "ankit";
   res.render("index", { title: "Express" });
+});
+
+router.get("/test", function (req, res) {
+  console.log("Redis Value: ", req.session.username);
+  res.render("index", { title: "exnpress" });
 });
 
 router.post("/register", registerInitialChecks, register);
